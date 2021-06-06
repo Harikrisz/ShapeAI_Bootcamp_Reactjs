6b437334c9170f25a4f7f02a30bbf8d644732ccf
Harikrisz <84263201+Harikrisz@users.noreply.github.com>
2021-06-06 12:44:19 +0530

    Add files via upload
    # SHAPEAI Javascript and Reactjs BOOTCAMP
    Hi I made this project during the 7 Days Free Bootcamp, conducted by <b> SHAPEAI
    </b>.
    The instructor during the session was Mr. Shaurya Sinha (Data Analyst Intern at Jio). I got to
    learn a lot during these 7 days and it was an amazing experience learning with SHAPEAI.
    <br><br>Here's the link for you to watch the sessions as well<br>
    <a href="https://www.youtube.com/playlist?list=PL7zl8TDRnbulLetcbkthT0p_IzwgRAYbu"> <img src="https://github.com/ShapeAI/PYTHON-AND-DATA-ANALYTICS/blob/main/YOUTUBE%20THUMBNAIL-4.png"> </a>
    <br>I got to have hands on experience on:
    <li>JavaScript
    <li>HTML
    <li>React.js
    <br>during these 7 days, and everything was explained from the very basics so that
    anyone with zero experience on programming can learn.
    I enjoyed these 7 days, you can as well. To register for next free 7 days bootcamp, visit:
    <a href="https://www.shapeai.tech"> www.shapeai.tech</a>
    or follow SHAPEAI on:
    <li><a href=
    "https://in.linkedin.com/company/shapeai">LinkedIn</a>
    <li><a href=
    "https://www.instagram.com/shape.ai/?hl=en">Instagram</a>
    <li><a
    href=
    "https://www.youtube.com/channel/UCTUvDLTW9meuDXWcbmISPdA">YouTu
    be</a>
    <li><a href=
    "https://github.com/shapeai">GitHub</a>

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Laptops Under 50k</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+
+function Note() {
+  return (
+    <>
+      <div className="note">
+        <h1>Dell Inspiron 5570</h1>
+        <p>
+          The Dell Inspiron 5570 is a Windows 10 laptop with a 15.60-inch
+          display. It is powered by a 8th Gen Intel Core i7 processor clocked at
+          1.6GHz. Other specifications include 4GB of RAM, a 1TB HDD, AMD Radeon
+          530 Graphics graphics, and a 2.20kg weight.
+          <br />
+          <b>Price Rs.49,990</b>
+        </p>
+      </div>
+      <div className="note">
+        <h1>Asus VivoBook S510UN-BQ217T</h1>
+        <p>
+          The Asus VivoBook S510UN-BQ217T is a Windows 10 laptop with a
+          15.60-inch display. An Intel Core i5 processor clocked at 1.6GHz.
+          Other features include 8GB of RAM, a 1TB HDD, Nvidia GeForce MX150
+          graphics, and a 1.70kg weight.
+          <br />
+          <b>Price Rs.49,990</b>
+        </p>
+      </div>
+      <div className="note">
+        <h1>HP 15-DA0070TX</h1>
+        <p>
+          The HP 15-DA0070TX is a Windows 10 laptop with a 15.60-inch display.
+          It is powered by an Intel Core i3 processor. Other features include
+          8GB of RAM, and a 1TB HDD. HP 15-DA0070TX Laptop (Windows 10, 8GB RAM,
+          1000GB HDD, Intel Core i3, Black, 15.6 inch) <br />
+          <b>Price Rs.49,279</b>
+        </p>
+      </div>
+      <div className="note">
+        <h1>Asus VivoBook E203MA-YS03</h1>
+        <p>
+          The Asus VivoBook E203MA-YS03 is a Windows 10 laptop with a 11.60-inch
+          display. An Intel Celeron Dual Core processor clocked at 1.1GHz. Other
+          features include 4GB of RAM, a 64GB of storage, and a 0.90kg weight.{" "}
+          <br />
+          <b>Price Rs.48,878</b>
+        </p>
+      </div>
+      <div className="note">
+        <h1>Acer ChromeBook</h1>
+
+        <p>
+          The Acer ChromeBook is a Chrome OS laptop with a 14.00-inch display.
+          It is powered by an Intel Celeron Dual Core processor. Other
+          specifications include 4GB of RAM, a 32GB HDD, Intel Integrated HD
+          Graphics graphics, and a 1.44kg weight. <br />
+          <b>Price Rs.47,663</b>
+        </p>
+      </div>
+      <div className="note">
+        <h1>Lenovo ThinkPad E480</h1>
+        <p>
+          The Lenovo ThinkPad E480 is a Windows 10 laptop with a 14.00-inch
+          display. It is powered by an Intel Core i7 processor clocked at
+          1.8GHz. Other specifications include 16GB of RAM, a 500GB HDD, and a
+          1.75kg weight. Lenovo Thinkpad E480 Laptop (Linux, 4GB RAM, 500GB HDD,
+          Intel Core i3, Black, 14.0 Inch) <br />
+          <b>Price Rs.46,990</b>
+        </p>
+      </div>
+      <div className="note">
+        <h1>Lenovo ThinkPad E460</h1>
+        <p>
+          The Lenovo ThinkPad E460 is a DOS laptop with a 14.00-inch display. It
+          is powered by a 6th Gen Intel Core i5 processor. Other specifications
+          include 4GB of RAM, a 1TB HDD, AMD Radeon R7 M360 graphics, and a
+          1.90kg weight. Lenovo Thinkpad E460 Laptop (DOS, 4GB RAM, 1000GB HDD,
+          Intel Core i3, Black, 14 inch)
+          <br />
+          <b>Price Rs.46,800</b>
+        </p>
+      </div>
+      <div className="note">
+        <h1>HP Pavilion 14-BF013TU</h1>
+        <p>
+          The HP Pavilion 14-BF013TU is a Windows 10 Home laptop with a
+          14.00-inch display. A 7th Gen Intel Core i3 processor clocked at
+          2.4GHz. Other features include 4GB of RAM, a 1TB HDD, Intel Integrated
+          HD Graphics 620 graphics, and a 1.62kg weight.(Windows 10, 16GB RAM,
+          512GB SSD, Intel Core i5, 11th Gen, Silver, 13.3 inch) <br />
+          <b>Price Rs.44,999</b>
+        </p>
+      </div>
+      <div className="note">
+        <h1>Xiaomi Mi Notebook 14 (IC)</h1>
+        <p>
+          The Xiaomi Mi Notebook 14 (IC) is a Windows 10 Home laptop with a
+          14.00-inch display and a Everyday Use form factor. A 10th Gen Intel
+          Core i5 processor. The Xiaomi laptop was launched in January 2021.
+          Other features include 8GB of RAM, a 512GB SSD, a 46Whr battery, and a
+          1.50kg weight.
+          <br />
+          <b>Price Rs.43,999</b>
+        </p>
+      </div>
+      <div className="note">
+        <h1>Smartron T Book Flex T1223</h1>
+        <p>
+          The Smartron T Book Flex T1223 is a Windows 10 Home laptop with a
+          12.50-inch touchscreen display. An Intel Core m3 processor. Other
+          features include 4GB of RAM, a 128GB SSD, Intel Integrated HD Graphics
+          graphics, and a 1.38kg weight. (Win 10, 4GB RAM, 128GB HDD, Intel m3,
+          Orange, 12.2 inch) <br />
+          <b>Price Rs.42,490</b>
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Note;
